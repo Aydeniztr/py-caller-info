dec5ec3760789173f2b8623c3732e3c5ee9f401d
Aydeniztr <88689203+Aydeniztr@users.noreply.github.com>
2022-02-06 00:53:45 +0300

    Add files via upload

---
+++ tracker.py
@@ -0,0 +1,157 @@
+from phonenumbers import carrier,timezone
+import phonenumbers # mem_info(20100kb)
+import sys
+import os
+
+
+banner = '''
+  .oooooo.             oooo  oooo                              o8o               .o88o.           
+ d8P'  `Y8b            `888  `888                              `"'               888 `"           
+888           .oooo.    888   888   .ooooo.  oooo d8b         oooo  ooo. .oo.   o888oo   .ooooo.  
+888          `P  )88b   888   888  d88' `88b `888""8P         `888  `888P"Y88b   888    d88' `88b 
+888           .oP"888   888   888  888ooo888  888     8888888  888   888   888   888    888   888 
+`88b    ooo  d8(  888   888   888  888    .o  888              888   888   888   888    888   888 
+ `Y8bood8P'  `Y888""8o o888o o888o `Y8bod8P' d888b            o888o o888o o888o o888o   `Y8bod8P' 
+                                                                                                  
+author: Ahmet Yigit AYDENIZ                                                                                               
+                                                                                                  
+'''
+
+msg = '''
+
+info:
+
+
+
+
+'''
+
+usg = '''
+
+costum argumants:
+
+	'--help,-h'
+
+	'--list'
+
+	'--location'
+
+'''
+
+# setting up ANSI colors:
+
+# black   = '\033[30m'
+# red     = '\033[31m'
+# green   = '\033[32m'
+# yellow  = '\033[33m'
+# blue    = '\033[34m'
+# magenta = '\033[35m'
+# cyan    = '\033[36m'
+# white   = '\033[37m'
+
+
+def black():
+	print('\033[30m')
+
+def red():
+	print('\033[31m')
+
+def green():
+	print('\033[32m')
+
+def yellow():
+	print('\033[33m')
+
+def blue():
+	print('\033[34m')
+
+def magenta():
+	print('\033[35m')
+
+def cyan():
+	print('\033[36m')
+
+def white():
+	print('\033[37m')
+	
+def reset():
+	print('\033[0m')
+	
+#some functions to make the code easier to write
+#I am not gonna explain them they are all self explanatory
+	
+def generate_maps_link(text,gps):
+
+	gps_query = '/@'+ gps
+	maps = 'https://www.google.com/maps/place/'+ text + gps
+	 
+	print ('\nsee the location in google maps:\n'+ maps)
+	
+
+def get_info(phone_number):
+	
+	print('\n')
+	
+	print(banner)
+
+	ni = phonenumbers.parse(str(phone_number), GB)
+	print (ni)
+	
+	sp = carrier.name_for_number(ni, "en")
+	print(sp)
+	
+	tz = timezone.time_zones_for_number(ni)
+	print(tz)
+	
+	generate_maps_link(tz)
+
+	print('\n')
+
+
+
+if len(sys.argv) <= 1:
+	
+	print(banner + msg)
+
+else:
+
+	print(banner)
+	phone_number = sys.argv[1]
+	get_info(phone_number)
+
+elif sys.argv[1] == '--list'
+
+	print(banner)
+	
+	while True:
+		
+		try:
+			
+			choice = input('Do you want to save the output in the output-logs.txt file ? (y/n):')
+			
+			with open ("list.txt", "r") as mylist:
+				data = mylist.read().splitlines()
+
+				for i in data:
+					
+					get_info(i)
+					
+					if choice == 'y'
+					with open("output.txt", "a") as f:
+    					print(get_info(i), file=f)
+				
+		except:
+			
+			print('there in no file named list.txt or the file \nis empty.(please check the file)')
+			
+				
+				
+elif sys.argv[1] == '--help' or sys.argv[1] == '-h'
+
+	print(banner + usage)
+
+elif sys.argv[1] == '--location'
+
+	gps == sys.argv[2]
+	print(banner)
+	generate_maps_link(text,gps)
